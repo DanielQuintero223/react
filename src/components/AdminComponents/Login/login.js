99f4a4df617a52caf041266af81940886d7567fa
DanielQuintero223 <100146874+DanielQuintero223@users.noreply.github.com>
2022-06-06 12:13:35 -0500

    Add files via upload

--- src/components/AdminComponents/Login/login.js
+++ src/components/AdminComponents/Login/login.js
@@ -1,11 +1,45 @@
-import React from "reacto";
-import { Form, Button, Input } from "antd";
+import React, { useState} from "react";
+import { Form, Button, Input, notification  } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import "./login.scss";
+import { ACCESS_TOKEN , REFRESH_TOKEN } from "../../../api/constants";
 
 export default function SignIn() {
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
+    });
+
+    const changeForm = e => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    }
+    const login = async e => {
+        e.preventDefault();
+        console.log(inputs)
+        const result = await SignIn(inputs);
+        if(result.message){
+            notification["error"]({
+                message: result.message
+            });
+        } else{
+            const { accessToken, refreshToken } = result;
+            localStorage.setItem(ACCESS_TOKEN, accessToken);
+            localStorage.setItem(REFRESH_TOKEN, refreshToken);
+
+            notification["success"]({
+                message:"login correcto"
+            });
+
+            window.location.href = "/admin";
+        }
+        console.log(result);
+    };
+
     return(
-        <Form className="login-form">
+        <Form className="login-form" onChange={changeForm}>
             <Form.Item>
                 <Input
                 prefix={
@@ -29,7 +63,7 @@ export default function SignIn() {
                 />
             </Form.Item>
             <Form.Item>
-                <Button htmlType="submit" className="login-form__button">
+                <Button htmlType="submit" className="login-form__button" onClick={login}>
                     Entrar
                 </Button>
             </Form.Item>
